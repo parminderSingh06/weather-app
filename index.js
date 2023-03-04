18544bcc4608ff689179442a8aa884d5c4ba8a6c
Parminder Singh <parmindersingh1086@gmail.com>
2023-03-03 19:22:38 -0500

    Added weather display to the web rather than log

--- index.js
+++ index.js
@@ -1,36 +1,59 @@
-const apiCalls = (function(){
-async function getLonLat(){
-    try{
-        const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${displayController.city.value},${displayController.state.value},${displayController.country.value}&appid=41de0b2033f4a4897e9a51f99316c532`,{mode: "cors"});
-        const locationData = await response.json();
-        getWeatherData(locationData[0].lat, locationData[0].lon);
-    } catch(error){
-        console.log('LOCATION NO WORK SRY.');
+const temp = (function(){
+    function toFahrenheit(kelvin){
+        let fahrenheitTemp = (1.8 * (kelvin-273.15) + 32);
+        return fahrenheitTemp.toFixed(0);
     }
-}
 
-async function getWeatherData(lat, lon){
-    try{
-        console.log(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=41de0b2033f4a4897e9a51f99316c532`);
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=41de0b2033f4a4897e9a51f99316c532`, {mode:"cors"});
-        const weatherData = await response.json();
-        console.log(weatherData);
-    }catch(error){
-        console.log("WEATHER NO WORK SRY.");
+    function toCelsius(kelvin){
+        let celsiusTemp = (kelvin - 273.15);
+        return celsiusTemp.toFixed(0);
     }
-}
-return{getLonLat};
+
+    return {toFahrenheit, toCelsius};
+})();
+
+const apiCalls = (function(){
+    async function getWeatherData(city){
+        try{
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=41de0b2033f4a4897e9a51f99316c532`, {mode:"cors"});
+            const weatherData = await response.json();
+            console.log(weatherData);
+            return weatherData;
+        }catch(error){
+            console.log("WEATHER NO WORK SRY.");
+        }
+    }
+
+    return{getWeatherData};
 })();
 
 const displayController = (function(){
+    let data;
     const form = document.querySelector('form');
     const city = document.querySelector('#citySearch');
-    const state = document.querySelector('#stateSearch');
-    const country = document.querySelector('#countrySearch');
-    form.addEventListener('submit', function(e){
+    const cityDisplay = document.querySelector('#cityD');
+    const countryDisplay = document.querySelector('#countryD');
+    const tempDiv = document.querySelector('#temp');
+    form.addEventListener('submit', async function(e){
         e.preventDefault();
-        apiCalls.getLonLat();
+        data =  await apiCalls.getWeatherData(city.value);
+        displayLocation();
+        displayTemp();
         form.reset();
     });
-    return{city, state, country};
+
+    function displayLocation(){
+        console.log(data.name);
+        cityDisplay.innerHTML = `${data.name}`;
+        if(data.sys.country == 'US')
+        {
+            data.sys.country = 'United States';
+        }
+        countryDisplay.innerHTML = `${data.sys.country}`;
+    }
+
+    function displayTemp(){
+        tempDiv.innerHTML = `${temp.toFahrenheit(data.main.temp)}°`;
+    }
+    return{};
 })();
